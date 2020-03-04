baa932f10d6055f730743ba5b1026992382cc724
Rohan-Wankhede <45092885+Rohan-Wankhede@users.noreply.github.com>
2020-03-04 09:08:00 +0530

    make it better

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ng-nytimes-demo'));
+app.use(express.static('./dist/NewYorkArticles'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'/dist/ng-nytimes-demo/index.html'));
+res.sendFile(path.join(__dirname,'/dist/NewYorkArticles/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
